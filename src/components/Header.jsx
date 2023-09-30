4fad11d4c92ef1b34e9bbb020776266709340ada
mconil <mconil5988@gmail.com>
2023-09-30 17:09:05 -0400

    routes added to pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-// import React from 'react';
-// import Nav from './Nav';
-
-// const Header = () => {
-//   return (
-//     <div style={{display: 'flex',justifyContent: 'space-between', alignItems: 'center', fontSize: '35px', color: 'white', textTransform: 'uppercase', textAlign: 'left', padding: '0 20px'}}>
-//       <h1>Nom@d</h1>
-//       <Nav/>
-//     </div>
-//   );
-// };
-=======
 import React from 'react';
 import Nav from './Nav';
  import {Link} from "react-router-dom"
@@ -25,6 +12,5 @@ import Nav from './Nav';
         </nav>
       </header>
  }
->>>>>>> 22582ba (routes added to pages)
 
 // export default Header;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-// import React from "react";
-// import { Link } from "react-router-dom";
-
-// const Nav = (props) => {
-//   return (
-//     <div className="nav">
-
-//       {/* <Link to="/"> */}
-//         <img src="/images/HomeIcon.png" alt="Home Icon" style={{ width: "50px", height: "50px", marginRight: "10px" }}/>
-//       {/* </Link> */}
-
-//       {/* <Link to="/post"> */}
-//         <img src="/images/PostIcon.png" alt="Post Icon" style={{ width: "40px", height: "40px", marginRight: "10px" }}/>
-//       {/* </Link> */}
-
-//       {/* <Link to="/profile"> */}
-//         <img src="/images/ProfileIcon.png" alt="Profile Icon" style={{ width: "40px", height: "40px", marginRight: "10px" }}/>
-//       {/* </Link> */}
-
-//       {/* <Link to="/logout"> */}
-//         <img src="/images/LogoutIcon.png" alt="Logout Icon" style={{ width: "60px", height: "60px", marginRight: "10px" }}/>
-//       {/* </Link> */}
-//     </div>
-//   );
-// };
-
-// export default Nav;
-=======
 import React from "react";
 import Header from "../components/Header"
 import { Outlet } from "react-router-dom";
@@ -64,6 +35,5 @@ return (
 };
 
 export default Nav;
->>>>>>> 22582ba (routes added to pages)
 
 
