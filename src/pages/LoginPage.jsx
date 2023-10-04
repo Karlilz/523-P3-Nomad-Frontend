f712ec7d668405355e555123a95601ca24c6feeb
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-10-04 13:49:16 -0400

    more cleanup

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,11 +7,8 @@ const LoginPage = () => {
     const [redirect,setRedirect] =useState(false);
     async function login(e){
         e.preventDefault();
-<<<<<<< HEAD
-       const response=  await fetch('http://localhost:4000/login',{
-=======
+
        const response=  await fetch('http://localhost:3000/login',{
->>>>>>> 08139bd (added loginpage,signup,app.js)
             method:'POST',
             body: JSON.stringify({username,password}),
             headers:{'Content-Type':'application/json'},
