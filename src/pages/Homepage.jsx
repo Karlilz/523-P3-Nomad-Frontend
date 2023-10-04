8c38d54ff05ace684d00afa377141f9024c11707
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-10-04 14:01:50 -0400

    test

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,26 @@
+
+// import React from 'react';
+// import seedData from './data.json'; 
+// import Nav from '../components/Nav';
+
+// const Homepage = () => {
+//   return (
+//     <div>
+//       <h1>Nom@d</h1>
+//       <Nav/>
+//       <div className="card-container">
+//         {seedData.map((item, index) => (
+//           <div className="card" key={index}>
+//             <img src={item.image} alt={item.title} />
+//             <h2>{item.title}</h2>
+//             <p>Location: {item.location}</p>
+//             <p>Username: {item.username}</p>
+//             <p>{item.caption}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Homepage;
